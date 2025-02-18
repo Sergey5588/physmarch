de39631487ed49c689b9033d7a1587ba9e1c14d8
MAX-TS <68846406+MAX-TS@users.noreply.github.com>
2025-02-18 21:04:52 +0300

    fixed last merge

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
         // make sure the viewport matches the new window dimensions; note that width and 
         // height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
-	glUniform2f(glGetUniformLocation(shaderProgram.ID, "Resolution"), 800.0f, 800.0f);
 	WIDTH = width;
 	HEIGHT = height;
 }
